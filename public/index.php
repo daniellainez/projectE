9c1a1edb035fa2e4926a068a8837f39e9c520cf9
daniel <daniellainez83@gmail.com>
2020-11-12 04:41:15 -0600

    Commit Daniel

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__.'/../bootstrap/init.php';
+
+$app_name = getenv('APP_NAME');
+echo $app_name;
+
+
+$Jose = "Jose";
+echo $Jose;
+
+$prueba = "Hola desde myRepo";
+echo $prueba;
+$dereck = "Dereck";
+echo $dereck;
+
+$daniel = "Daniel";
+echo $daniel;
+?>
